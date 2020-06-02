e1915285a91551b42040de0f6c8b6f2c06fea411
ihor <krohmal.igor777@gmail.com>
2020-06-02 17:53:17 +0300

    implementation solution 30

---
+++ src/main/java/spring/intro/dao/impl/UserDaoImpl.java
@@ -0,0 +1,50 @@
+package spring.intro.dao.impl;
+
+import java.util.List;
+import javax.persistence.TypedQuery;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import spring.intro.dao.UserDao;
+import spring.intro.model.User;
+
+@Repository
+public class UserDaoImpl implements UserDao {
+
+    @Autowired
+    private SessionFactory sessionFactory;
+
+    @Override
+    public void add(User user) {
+        //sessionFactory.openSession().save(user);
+        Session session = null;
+        Transaction transaction = null;
+        try {
+            session = sessionFactory.openSession();
+            transaction = session.beginTransaction();
+            session.save(user);
+            transaction.commit();
+        } catch (Exception e) {
+            if (transaction != null) {
+                transaction.rollback();
+            }
+            throw new RuntimeException("Can't insert user entity", e);
+        } finally {
+            if (session != null) {
+                session.close();
+            }
+        }
+    }
+
+    @Override
+    public List<User> listUsers() {
+        try (Session session = sessionFactory.openSession()) {
+            TypedQuery<User> query = session.createQuery("from User", User.class);
+            return query.getResultList();
+        } catch (Exception e) {
+            throw new RuntimeException("Error retrieving all users", e);
+        }
+    }
+}
